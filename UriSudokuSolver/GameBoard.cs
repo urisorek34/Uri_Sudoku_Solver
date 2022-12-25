9ec82f89a609cae29509bc07f0a2d5640c770048
urisorek <urisorek34@gmail.com>
2022-12-25 19:08:37 +0200

    add abstract class GameBoard

---
+++ UriSudokuSolver/GameBoard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriSudokuSolver
+{
+    /*Represent abstract game board*/
+    abstract class GameBoard<T>
+    {
+        
+        private T[,] board;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameBoard{T}"/> class.
+        /// </summary>
+        public GameBoard(int size)
+        {
+            board = new T[size, size];
+        }
+
+        public GameBoard(int rows, int cols)
+        {
+            board = new T[rows, cols];
+        }
+
+        /*copy constractor*/
+        public GameBoard(T[,] board)
+        {
+            this.board = new T[board.GetLength(0), board.GetLength(1)];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    this.board[i, j] = board[i, j];
+                }
+            }
+        }
+
+        /*Indexer for the game board.*/
+        public T this[int row, int col]
+        {
+            get
+            {
+                return board[row, col];
+            }
+            set
+            {
+                board[row, col] = value;
+            }
+        }
+    }
+}
