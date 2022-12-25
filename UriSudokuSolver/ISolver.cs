da842a74aef4922e79c8c167c75111f15da78ae5
urisorek <urisorek34@gmail.com>
2022-12-25 19:58:51 +0200

    Create ISolver interface

---
+++ UriSudokuSolver/ISolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriSudokuSolver
+{
+    /// <summary>
+    /// Interface for solver of a game.
+    /// </summary>
+    interface ISolver
+    {
+        public void Solve();
+    }
+}
