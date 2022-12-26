b8c6d36b8a164eabdfb4ac443ddc25ff5b50e307
urisorek <urisorek34@gmail.com>
2022-12-26 14:07:30 +0200

    add SudokuSolver class
    solves a given board using optimized backtracking.

---
+++ UriSudokuSolver/SudokuSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriSudokuSolver
+{
+    class SudokuSolver : ISolver
+    {
+        private SudokuBoard board;
+        private int minValue;
+        private int maxValue;
+        private int rows;
+        private int cols;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SudokuSolver"/> class.
+        /// </summary>
+        /// <param name="board">The board.</param>
+        public SudokuSolver(SudokuBoard board)
+        {
+            this.board = board;
+            minValue = board.GetMinValue();
+            maxValue = board.GetMaxValue();
+            rows = board.GetRows();
+            cols = board.GetCols();
+            
+        }
+       
+
+
+
+
+    }
+}
