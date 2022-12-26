88b36691002bb9e12eec35b4b9f642c5ed22f04d
urisorek <urisorek34@gmail.com>
2022-12-26 14:35:57 +0200

    add SudukuSolverUtillity static class
    utility class for tools for the sudoku solver.

---
+++ UriSudokuSolver/SudukuSolverUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriSudokuSolver
+{
+
+    /*Sudoku solver static utility class.*/
+    static class SudukuSolverUtility
+    {
+
+
+}
