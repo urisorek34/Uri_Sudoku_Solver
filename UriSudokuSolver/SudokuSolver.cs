6067b79755f92496fe3f2beaa8614bcec5145007
urisorek <urisorek34@gmail.com>
2022-12-26 20:41:22 +0200

    Main backtracking functions
    function that solves the sudoku board using backtracking (recursion).

--- UriSudokuSolver/SudokuSolver.cs
+++ UriSudokuSolver/SudokuSolver.cs
@@ -17,8 +17,55 @@ namespace UriSudokuSolver
         {
             this.board = board;
         }
-        
 
+        /*Function tries to solve the sudoku board.*/
+        public void Solve()
+        {
+            if (SolveOptimizedSudoku(SudukuSolverUtility.CacheValidValues(board)))
+            {
+                Console.WriteLine("Solved!");
+            }
+            else
+            {
+                Console.WriteLine("No solution!");
+            }
+        }
+        
+        /*Function Solves the sudoku board using backtracking.*/
+        private bool SolveOptimizedSudoku(Dictionary<(int, int), List<int>> cache)
+        {
+
+            (int, int) blanck = SudukuSolverUtility.FindFirstEmpty(board);
+            int row = blanck.Item1;
+            int col = blanck.Item2;
+            
+            // Recursion stoping condition.
+            if (row == board.GetRows())
+            {
+                return true;
+            }
+
+            // Go over all the options for each empty place in the board.
+            for (int value = 0; value < cache[blanck].Count; value++)
+            {
+
+                if (SudukuSolverUtility.IsLegalValue(board, row, col, cache[blanck][value]))
+                {
+                    board[row, col] = cache[blanck][value];
+
+                    if (SolveOptimizedSudoku(cache))
+                    {
+                        return true;
+                    }
+                    board[row, col] = 0;
+
+                }
+
+            }
+
+            return false;
+
+        }
 
 
 
