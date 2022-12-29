3f126b0362122b17486cabee94db40493dd9c327
uriso <urisorek34@gmail.com>
2022-12-29 17:28:32 +0200

    Add Board custom exceptions

---
+++ UriSudokuSolver/CustomExceptions/BoardNotValidException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriSudokuSolver
+{
+    /*Exception for when a board is not a valid board.*/
+    internal class BoardNotValidException : ArgumentException
+    {
+        
+        public BoardNotValidException(string message) : base(message)
+        {
+        }
+    }
+   
+}
