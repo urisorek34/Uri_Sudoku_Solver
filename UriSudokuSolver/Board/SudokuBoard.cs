82fa27e85e01e2af572b0068dce607ca2e25f397
uriso <urisorek34@gmail.com>
2023-01-01 18:02:22 +0200

    Bug fix in validator and remove cell class

--- UriSudokuSolver/Board/SudokuBoard.cs
+++ UriSudokuSolver/Board/SudokuBoard.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UriSudokuSolver.Board;
 using UriSudokuSolver.CustomExceptions;
 
 namespace UriSudokuSolver
